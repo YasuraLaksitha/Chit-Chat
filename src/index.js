afe1a263596d7ef469544f2ff86d3896c3f1d05f
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-12-08 20:31:30 +0530

    changed and integrated dep into socket.io;
    intialized web socket server

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(3000, '127.0.0.1', () => {
-    console.log('server running in port 3000')
-})
---
+++ src/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http')
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app)
+const io = new socketIO.Server(server)
+
+io.on('connection',socket=> {
+
+    console.info('web socket started')
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('server running in port 3000')
+})
